dd4236b3dcbb9ade74d6c2fe710396ca5cc5d3f7
gergo <info@gazdagergo.com>
2017-12-31 05:31:14 +0100

    cloud function hw [skip ci]

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+
+process.env.FIREBASE_PROJECT = JSON.stringify({
+  apiKey: "AIzaSyBfXtwHBhGS6UVEZiBMel2n79GQ8tPlqI8",
+  authDomain: "youtyuk-4e5b5.firebaseapp.com",
+  databaseURL: "https://youtyuk-4e5b5.firebaseio.com",
+  projectId: "youtyuk-4e5b5",
+  storageBucket: "youtyuk-4e5b5.appspot.com",
+  messagingSenderId: "699546239047"
+});
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.writeExample = functions.database.ref('/payments')
+  .onWrite(event => {
+    console.log(event.data);
+  });
+
